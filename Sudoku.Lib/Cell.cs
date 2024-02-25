70abbcd2306530e95505277c97c87d30f4145372
György Turbék <profgyuri@gmail.com>
2024-02-25 02:13:45 +0100

    Add project files.

---
+++ Sudoku.Lib/Cell.cs
@@ -0,0 +1,32 @@
+﻿namespace Sudoku.Lib;
+
+public class Cell
+{
+    public int Value { get; set; }
+    public PencilMarks PencilMarks { get; set; } = new PencilMarks();
+
+    public static implicit operator Cell(int value)
+    {
+        return new Cell { Value = value };
+    }
+
+    public static explicit operator int(Cell cell)
+    {
+        return cell.Value;
+    }
+}
+
+public class PencilMarks
+{
+    public int[] Marks { get; set; } = new int[9];
+
+    public bool IsOneVisible { get; set; } = false;
+    public bool IsTwoVisible { get; set; } = false;
+    public bool IsThreeVisible { get; set; } = true;
+    public bool IsFourVisible { get; set; } = false;
+    public bool IsFiveVisible { get; set; } = true;
+    public bool IsSixVisible { get; set; } = false;
+    public bool IsSevenVisible { get; set; } = true;
+    public bool IsEightVisible { get; set; } = true;
+    public bool IsNineVisible { get; set; } = false;
+}
---
+++ Sudoku.Lib/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Sudoku.Lib;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty] private Cell[,] _board = new Cell[9, 9];
+
+    public MainWindowViewModel()
+    {
+        Board = new Cell[9, 9]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 8, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 7, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 6, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 5, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 4, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 3, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 2, 0 }
+        };
+    }
+}
