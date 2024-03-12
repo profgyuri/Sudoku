cb04825bc1785d0303f5800689086681be3e99fe
György Turbék <profgyuri@gmail.com>
2024-03-12 20:33:32 +0100

    Made zeros invisible

--- Sudoku.Lib/Cell.cs
+++ Sudoku.Lib/Cell.cs
@@ -3,6 +3,7 @@
 public class Cell
 {
     public int Value { get; set; }
+    public bool IsValueVisible => Value != 0;
     public PencilMarks PencilMarks { get; set; } = new PencilMarks();
 
     public static implicit operator Cell(int value)
@@ -20,13 +21,26 @@ public class PencilMarks
 {
     public int[] Marks { get; set; } = new int[9];
 
-    public bool IsOneVisible { get; set; } = false;
-    public bool IsTwoVisible { get; set; } = false;
-    public bool IsThreeVisible { get; set; } = true;
-    public bool IsFourVisible { get; set; } = false;
-    public bool IsFiveVisible { get; set; } = true;
-    public bool IsSixVisible { get; set; } = false;
-    public bool IsSevenVisible { get; set; } = true;
-    public bool IsEightVisible { get; set; } = true;
-    public bool IsNineVisible { get; set; } = false;
+    public bool IsOneVisible { get; set; }
+    public bool IsTwoVisible { get; set; }
+    public bool IsThreeVisible { get; set; }
+    public bool IsFourVisible { get; set; }
+    public bool IsFiveVisible { get; set; }
+    public bool IsSixVisible { get; set; }
+    public bool IsSevenVisible { get; set; }
+    public bool IsEightVisible { get; set; }
+    public bool IsNineVisible { get; set; }
+
+    public void MakeAllInvisible()
+    {
+        IsOneVisible = false;
+        IsTwoVisible = false;
+        IsThreeVisible = false;
+        IsFourVisible = false;
+        IsFiveVisible = false;
+        IsSixVisible = false;
+        IsSevenVisible = false;
+        IsEightVisible = false;
+        IsNineVisible = false;
+    }
 }
